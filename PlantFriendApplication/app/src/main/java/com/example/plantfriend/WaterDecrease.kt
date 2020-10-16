9f69f5796f0b7b6192c6b9e3e9f143b4241d95c5
Helene <helene2k@live.nl>
2020-10-16 09:11:18 +0200

    probleem opgelost met een handler

--- PlantFriendApplication/app/src/main/java/com/example/plantfriend/WaterDecrease.kt
+++ PlantFriendApplication/app/src/main/java/com/example/plantfriend/WaterDecrease.kt
@@ -1,31 +1,27 @@
 package com.example.plantfriend
 
-import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import java.io.File
 
-class WaterDecrease: BroadcastReceiver() {
+class WaterDecrease : BroadcastReceiver() {
     val fileName = "PlantData"
     lateinit var context: Context
     var file: File? = null
-    lateinit var waterLevel: WaterLevel
     override fun onReceive(context:Context, intent:Intent) {
-        waterLevel = intent.getSerializableExtra("water") as WaterLevel
-            file = File(context.filesDir, fileName)
-            this.context = context
-            var content: String? = null
-            context.openFileInput(fileName).use { stream ->
-                content = stream.bufferedReader().use {
-                    it.readText()
-                }
-            }
-            if (content?.toInt()!! >= 10) {
-                context.openFileOutput(fileName, Context.MODE_PRIVATE).use {
-                    it.write((content?.toInt()!! - 10).toString().toByteArray())
-                }
-                waterLevel.water.postValue(content?.toInt()!! - 10)
+        file = File(context.filesDir, fileName)
+        this.context = context
+        var content: String? = null
+        context.openFileInput(fileName).use { stream ->
+            content = stream.bufferedReader().use {
+                it.readText()
             }
         }
+        if (content?.toInt()!! >= 10) {
+            context.openFileOutput(fileName, Context.MODE_PRIVATE).use {
+                it.write((content?.toInt()!! - 10).toString().toByteArray())
+            }
+        }
+    }
     }
